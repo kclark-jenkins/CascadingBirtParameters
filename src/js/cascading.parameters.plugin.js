889fee4e5533ba667397802fe6f071cec7c44bac
Kristopher Clark <krisclarkdev@krisbox.org>
2016-11-23 02:54:57 -0600

    Cleaned up code

--- src/js/cascading.parameters.plugin.js
+++ src/js/cascading.parameters.plugin.js
@@ -1,5 +1,5 @@
 (function ($) {
-    $.CascadingBirtParameters = function (options) {
+    $.CascadingBirtParameters = function (userOptions) {
 
         var defaults = {
                 report:          null,
@@ -11,7 +11,7 @@
                 }
             },
             plugin  = this,
-            options = options || {};
+            options = userOptions || {};
 
 
         plugin.init = function () {
@@ -29,76 +29,71 @@
             var myParameters = new actuate.Parameter(options.parametersDivID);
                 myParameters.setReportName(options.designName);
 
-            try {
-                myParameters.submit(function() {
-                    myParameters.downloadParameters(function(test) {
-                        var reportNameValuePair = new actuate.parameter.NameValuePair(test);
-                        //console.log(reportNameValuePair);
-                        for(i=0; i<reportNameValuePair.getName().length; i++) {
-                            var obj = new Object(reportNameValuePair.getName()[i]['_']);
+            myParameters.submit(function() {
+                myParameters.downloadParameters(function(test) {
+                    var reportNameValuePair = new actuate.parameter.NameValuePair(test);
+                    //console.log(reportNameValuePair);
+                    for(i=0; i<reportNameValuePair.getName().length; i++) {
+                        var obj = new Object(reportNameValuePair.getName()[i]['_']);
 
-                            var nameValueList = {
-                                    names: [],
-                                    values: []
-                            };
+                        var nameValueList = {
+                            names: [],
+                            values: []
+                        };
 
-                            for(j=0; j<obj['_selectNameValueList'].length;j++){
-                                var current = obj['_selectNameValueList'][j]['_'];
-                                nameValueList.names.push(current['_name']);
-                                nameValueList.values.push(current['_value']);
-                            }
-
-
-                            var currentObject = {
-                                metadata: {
-                                    cascadingParentName: obj['_cascadingParentName'],
-                                    columnType: obj['_columnType'],
-                                    controlType: obj['_controlType'],
-                                    dataType: obj['_dataType'],
-                                    defaultValue: obj['_defaultValue'],
-                                    displayName: obj['_displayName'],
-                                    group: obj['_group'],
-                                    groupPromptText: obj['_groupPromptText'],
-                                    helpText: obj['_helpText'],
-                                    isAdHoc: obj['_isAdHoc'],
-                                    isDynamicSelectionList: obj['_isDynamicSelectionList'],
-                                    isHidden: obj['_isHidden'],
-                                    isPassword: obj['_isPassword'],
-                                    isRequired: obj['_isRequired']
-                                },
-                                currentValue: obj['_currentValue'],
-                                currentName: obj['_currentName'],
-                                nameValueList: nameValueList
-                            };
-
-                            map[obj['_name']] = currentObject;
+                        for(j=0; j<obj['_selectNameValueList'].length;j++){
+                            var current = obj['_selectNameValueList'][j]['_'];
+                            nameValueList.names.push(current['_name']);
+                            nameValueList.values.push(current['_value']);
                         }
 
-                        for(var key in options.parameterInputMap) {
-                            $('#'+options.parameterInputMap[key]).empty();
+
+                        map[obj['_name']] = {
+                            metadata: {
+                                cascadingParentName: obj['_cascadingParentName'],
+                                columnType: obj['_columnType'],
+                                controlType: obj['_controlType'],
+                                dataType: obj['_dataType'],
+                                defaultValue: obj['_defaultValue'],
+                                displayName: obj['_displayName'],
+                                group: obj['_group'],
+                                groupPromptText: obj['_groupPromptText'],
+                                helpText: obj['_helpText'],
+                                isAdHoc: obj['_isAdHoc'],
+                                isDynamicSelectionList: obj['_isDynamicSelectionList'],
+                                isHidden: obj['_isHidden'],
+                                isPassword: obj['_isPassword'],
+                                isRequired: obj['_isRequired']
+                            },
+                            currentValue: obj['_currentValue'],
+                            currentName: obj['_currentName'],
+                            nameValueList: nameValueList
+                        };
+                    }
+
+                    for(var key1 in options.parameterInputMap) {
+                        $('#'+options.parameterInputMap[key1]).empty();
+                    }
+
+                    for(var key2 in map) {
+                        var parameterInput = $('#'+options.parameterInputMap[key2]);
+                        var currentValue  = map[key2]['currentValue'];
+                        //var currentName   = map[key2]
+                        var nameValueList1 = map[key2]['nameValueList'];
+                        var metaData      = map[key2]['metadata'];
+
+                        for(i=0;i<nameValueList1.names.length;i++){
+                                //console.log(options.parameterInputMap[key2]);
+                                parameterInput.append($('<option>', {
+                                    value: nameValueList1.values[i],
+                                    text: nameValueList1.names[i]
+                                }
+                            ));
                         }
-
-                        for(var key in map) {
-                            var currentValue  = map[key]['currentValue'];
-                            //var currentName   = map[key]
-                            var nameValueList = map[key]['nameValueList'];
-                            var metaData      = map[key]['metadata'];
-
-                            for(i=0;i<nameValueList.names.length;i++){
-                                //console.log(options.parameterInputMap[key]);
-                                $('#'+options.parameterInputMap[key]).append($('<option>', {
-                                    value: nameValueList.values[i],
-                                    text: nameValueList.names[i]
-                                }));
-                            }
-
-                            $('#'+options.parameterInputMap[key]).val(currentValue);
-                        }
-                    });
+                        parameterInput.val(currentValue);
+                    }
                 });
-            }catch(err){
-                console.log(err);
-            }
+            });
         };
 
         return this;
